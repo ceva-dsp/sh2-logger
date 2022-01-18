033f22cc71a2232be5afd63579e12fc45c4c646a
David Wheeler <david.wheeler@ceva-dsp.com>
2022-01-18 14:17:28 -0500

    Updated copyright dates.

--- DsfLogger.cpp
+++ DsfLogger.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-21 CEVA, Inc.
+ * Copyright 2018-2022 CEVA, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License and
